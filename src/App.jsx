98da6ddde1eadbad3db9b6e33daf09b7184c1fb6
Juan <93888821+john-9999@users.noreply.github.com>
2023-03-13 03:11:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InstruccionesCalorias from './components/InstruccionesCalorias'
+
+function App() {
+  return (
+    <div>
+      <InstruccionesCalorias></InstruccionesCalorias>
+    </div>
+  )
+}
+
+export default App
